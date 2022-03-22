1b2b8225c5f5d0dae838acabed432ac7efe587c8
Minsu <minus9521@gmail.com>
2022-03-22 15:15:50 -0400

    4th 42 cursus project - push_swap

---
+++ 04_PUSH_SWAP/sort/bubble.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int count)
+{
+    int temp;
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < count - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int numArr[10] = { 8, 4, 2, 5, 3, 7, 10, 1, 6, 9 };
+
+    bubble_sort(numArr, sizeof(numArr) / sizeof(int));
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", numArr[i]);
+    }
+    printf("\n");
+    return 0;
+}
