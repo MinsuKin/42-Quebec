0d21cc639f4aa595d1a6cebf21b17f53aab3559a
Minsu <minus9521@gmail.com>
2022-03-22 15:13:24 -0400

    2nd 42 cursus project - get_next_line

---
+++ 02_GET_NEXT_LINE/get_next_line.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/11 13:35:48 by minkim            #+#    #+#             */
+/*   Updated: 2022/01/30 15:03:44 by minkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_free(char *str)
+{
+	free(str);
+	return (NULL);
+}
+
+static char	*ft_return_line(char **buf)
+{
+	char	*address_n;
+	char	*temp;
+	char	*line;
+
+	if (!buf || !*buf)
+		return (NULL);
+	if (ft_strlen(*buf) == 0)
+		return (ft_free(*buf));
+	address_n = ft_strchr(*buf, '\n');
+	if (!address_n)
+	{
+		line = *buf;
+		*buf = 0;
+		return (line);
+	}
+	line = ft_strndup(*buf, address_n - *buf + 1);
+	if (!line)
+		return (NULL);
+	temp = ft_strndup(address_n + 1, ft_strlen(address_n + 1));
+	if (!temp)
+		return (NULL);
+	free(*buf);
+	*buf = temp;
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*save[OPEN_MAX];
+	char		buffer[BUFFER_SIZE + 1];
+	size_t		size;
+
+	if (read(fd, buffer, 0) == -1 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (ft_strchr(save[fd], '\n'))
+		return (ft_return_line(&save[fd]));
+	size = read(fd, buffer, BUFFER_SIZE);
+	while (size > 0)
+	{
+		buffer[size] = 0;
+		save[fd] = ft_strjoin(save[fd], buffer);
+		if (ft_strchr(save[fd], '\n'))
+			return (ft_return_line(&save[fd]));
+		size = read(fd, buffer, BUFFER_SIZE);
+	}
+	return (ft_return_line(&save[fd]));
+}
