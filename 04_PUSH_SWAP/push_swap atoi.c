abea0c37c047ad4c871adfef5bf57ec9fc6cd971
MinsuKin <minus9521@gmail.com>
2022-04-04 16:42:55 -0400

    22.04.02

--- 04_PUSH_SWAP/push_swap atoi.c
+++ 04_PUSH_SWAP/push_swap atoi.c
@@ -6,14 +6,13 @@
 /*   By: minsu <minsu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 13:52:24 by minkim            #+#    #+#             */
-/*   Updated: 2022/04/01 21:00:57 by minsu            ###   ########.fr       */
+/*   Updated: 2022/04/01 19:14:10 by minsu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 # include <stdio.h>
-# include <stdlib.h>
 
 int	ft_isspace(int c)
 {
@@ -50,69 +49,25 @@ int	ft_atoi(const char *str)
 	return (sign * n);
 }
 
-typedef struct NODE {
-	int data;
-	struct NODE *prev;
-	struct NODE *next;
-}NODE;
-// 이중 연결리스트 초기화
-void init(NODE *phead) {
-	phead->prev = phead;
-	phead->next = phead;
-    phead->data = 100;
-}
-// 이중 연결리스트 상태 출력
-void print_list(NODE *phead) {
-	NODE *p;
-    printf("%d | ", phead->data);
-	for (p = phead->next; p != phead; p = p->next) {
-		printf("%d | ", p->data);
-	}
-	printf("\n");
-}
-// 새로운 데이터를 노드 before의 오른쪽에 삽입
-void insert(NODE *before, int data) {
-	NODE *newnode = (NODE*)malloc(sizeof(NODE));
-	newnode->data = data;
-	newnode->prev = before;
-	newnode->next = before->next;
-	before->next->prev = newnode;
-	before->next = newnode;
-}
-// 노드 removed를 삭제
-void delete(NODE *head, NODE *removed) {
-	if (removed == head) return;
-	head->next = removed->next;
-	removed->next->prev = removed->prev;
-	free(removed);
-}
-// 메인
 int		main(int ac, char **av)
 {
+	int len;
+    int numArr[ac - 1];
     int i;
-    int len;
-    NODE *head = (NODE *)malloc(sizeof(NODE));
+    int j;
     
-    init(head);
-    print_list(head);
+    len = ac - 1;
     i = 1;
-    len = 0;
+    j = 0;
 
-    while (i < ac)
+    while (i <= len)
     {
-        insert(head, ft_atoi(av[i]));
-        print_list(head);
+        numArr[j] = ft_atoi(av[i]);
+        printf("%d ", numArr[j]);
         i++;
-        len++;
+        j++;
     }
-    len--;
-    while (len >= 0)
-    {
-        delete(head, head->next);
-        print_list(head);
-        len--;
-    }
-    
-    free(head);
+    printf("\n");
+
 	return (0);
 }
