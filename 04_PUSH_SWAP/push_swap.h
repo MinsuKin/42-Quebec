c7fda461f2dd3c377f0433635e959ce733d9fbe9
minsu <minus9521@gmail.com>
2022-04-04 16:42:55 -0400

    22.04.02

--- 04_PUSH_SWAP/push_swap.h
+++ 04_PUSH_SWAP/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
+/*   By: minsu <minsu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 13:52:21 by minkim            #+#    #+#             */
-/*   Updated: 2022/03/22 13:58:37 by minkim           ###   ########.fr       */
+/*   Updated: 2022/04/01 19:51:56 by minsu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-// typedef
+typedef struct	s_dllst
+{
+	int			    data;
+	struct s_dllst	*prev;
+	struct s_dllst	*next;
+}					t_stack;
+
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
+int		ft_isspace(int c);
 
 #endif
