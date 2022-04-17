6a856dcbb851670dec8a7c37c6feda0b6cd4b93c
Minsu <minus9521@gmail.com>
2022-04-17 14:45:46 -0400

    04.17.n

--- 04_PUSH_SWAP/push_swap.c
+++ 04_PUSH_SWAP/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 13:52:24 by minkim            #+#    #+#             */
-/*   Updated: 2022/04/17 09:26:32 by minkim           ###   ########.fr       */
+/*   Updated: 2022/04/17 14:44:41 by minkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,38 +23,69 @@ void print_list(Node *phead) {
 	printf("\n");
 }
 
-void ft_sorted(Node *ahead, Node *bhead, int size)
+int find_dup(Node *ahead, int dup)
 {
-	atob(ahead, bhead, size);
+	Node *p;
+
+	p = ahead->next;
+	while (p != ahead)
+	{
+		if (dup == p->data)
+			return 1;
+		p = p->next;
+	}
+	return 0;
 }
 
-// 메인
-int		main(int ac, char **av)
+int check_sorted(Node *a, int size)
 {
-    int i;
+	int i;
+	int arr[size];
+	Node *p;
+
+	p = a->next;
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = p->data;
+		i++;
+		p = p->next;
+	}
+	quickSort(arr, 0, size-1);
+	p = a->next;
+	i = 0;
+	while (i < size)
+	{
+		if (arr[i] != p->data)
+			return (0);
+		i++;
+		p = p->next;
+	}
+
+	return (1);
+}
+
+int ft_error()
+{
+	write(1, "Error\n", 7);
+	return (0);
+}
+
+int make_stack(int ac, char **av, Node *a, int i)
+{
 	int j;
 	int size;
-    Node *a = (Node *)malloc(sizeof(Node));
-    Node *b = (Node *)malloc(sizeof(Node));
-    
-    init(a);
-    init(b);
 
-    i = 1;
 	size = 0;
-
-    while (i < ac)
+    while (++i < ac)
     {
-		j = 0;
-		while (av[i][j])
+		j = -1;
+		while (av[i][++j])
 		{
-			if (av[i][j] == '-' && !ft_isdigit(av[i][j + 1]))
+			if (ft_isdigit(av[i][j]) || (av[i][j] == '-' && ft_isdigit(av[i][j + 1])))
 			{
-				write(1, "Error\n", 7);
-				return (0);
-			}
-			else if (ft_isdigit(av[i][j]) || (av[i][j] == '-' && ft_isdigit(av[i][j + 1])))
-			{
+				if (find_dup(a, ft_atoi(&av[i][j])) || ft_check_num(&av[i][j]))
+					return (ft_error());
 				insert(a->prev, ft_atoi(&av[i][j]));
 				size++;
 				while (ft_isdigit(av[i][j]) || av[i][j] == '-')
@@ -62,16 +93,62 @@ int		main(int ac, char **av)
 				j--;
 			}
 			else
-			{
-				write(1, "Error\n", 7);
-				return (0);
-			}
-			j++;
+				return (ft_error());
 		}
-		i++;
     }
+	return size;
+}
 
-    if (size == 2)
+// 메인
+int		main(int ac, char **av)
+{
+    int i;
+	// int j;
+	int size;
+    Node *a = (Node *)malloc(sizeof(Node));
+    Node *b = (Node *)malloc(sizeof(Node));
+    
+    init(a);
+    init(b);
+
+    i = 0;
+	// size = 0;
+
+	size = make_stack(ac, av, a, i);
+	if (size == 0)
+		return 0;
+
+    // while (i < ac)
+    // {
+	// 	j = 0;
+	// 	while (av[i][j])
+	// 	{
+	// 		if (ft_isdigit(av[i][j]) || (av[i][j] == '-' && ft_isdigit(av[i][j + 1])))
+	// 		{
+	// 			if (find_dup(a, ft_atoi(&av[i][j])) || ft_check_num(&av[i][j]))
+	// 			{
+	// 				write(1, "Error\n", 7);
+	// 				return (0);
+	// 			}
+	// 			insert(a->prev, ft_atoi(&av[i][j]));
+	// 			size++;
+	// 			while (ft_isdigit(av[i][j]) || av[i][j] == '-')
+	// 				j++;
+	// 			j--;
+	// 		}
+	// 		else
+	// 		{
+	// 			write(1, "Error\n", 7);
+	// 			return (0);
+	// 		}
+	// 		j++;
+	// 	}
+	// 	i++;
+    // }
+
+	if (check_sorted(a, size))
+		return (0);
+    else if (size == 2)
 		ft_two(a);
 	else if (size == 3)
 		ft_three(a);
@@ -86,7 +163,7 @@ int		main(int ac, char **av)
 	a->data = 0;
 	// print_list(a);
 	// print_list(b);
-
+	
 	ft_free(a);
 	ft_free(b);
 
--- 04_PUSH_SWAP/util.c
+++ 04_PUSH_SWAP/util.c
@@ -6,7 +6,7 @@
 /*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:38:19 by minkim            #+#    #+#             */
-/*   Updated: 2022/04/13 17:56:35 by minkim           ###   ########.fr       */
+/*   Updated: 2022/04/17 10:36:01 by minkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@ int	ft_isdigit(int c)
 
 int	ft_atoi(const char *str)
 {
-	int	n;
-	int	sign;
+	long	n;
+	long	sign;
+	long	res;
 
 	if (*str == 0)
 		return (0);
@@ -44,5 +45,37 @@ int	ft_atoi(const char *str)
 		n = n * 10 + (*str - '0');
 		str++;
 	}
-	return (sign * n);
+	res = sign * n;
+	if (res < -2147483648 || res > 2147483647)
+		return 0;
+	return ((int)res);
+}
+
+int	ft_check_num(const char *str)
+{
+	long	n;
+	long	sign;
+	long	res;
+
+	if (*str == 0)
+		return (0);
+	sign = 1;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	n = 0;
+	while (ft_isdigit(*str))
+	{
+		n = n * 10 + (*str - '0');
+		str++;
+	}
+	res = sign * n;
+	if (res < -2147483648 || res > 2147483647)
+		return 1;
+	return (0);
 }
