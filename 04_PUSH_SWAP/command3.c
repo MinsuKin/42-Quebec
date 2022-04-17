d81559cc62748842ea4773a7356e41aacf03c144
Minsu <minus9521@gmail.com>
2022-04-17 09:27:32 -0400

    04.17.m

--- 04_PUSH_SWAP/command3.c
+++ 04_PUSH_SWAP/command3.c
@@ -6,7 +6,7 @@
 /*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:41:54 by minkim            #+#    #+#             */
-/*   Updated: 2022/04/14 18:34:06 by minkim           ###   ########.fr       */
+/*   Updated: 2022/04/16 17:20:11 by minkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@ void reverse_rotate_a(Node *ahead)
 {
 	insert(ahead, ahead->prev->data);
 	delete_tail(ahead, ahead->prev);
-	write(1, "rra\n", 4);
+	// write(1, "rra\n", 4);
+	print_cmd(ahead, 7);
 }
 // rrb : reverse rotate b - 스택 b의 모든 원소들을 아래로 1 인덱스 만큼 내린다. 마지막 원소(바텀)는 첫 번째 원소(탑)가 된다.
-void reverse_rotate_b(Node *bhead)
+void reverse_rotate_b(Node *ahead, Node *bhead)
 {
 	insert(bhead, bhead->prev->data);
 	delete_tail(bhead, bhead->prev);
-	write(1, "rrb\n", 4);
+	// write(1, "rrb\n", 4);
+	print_cmd(ahead, 8);
 }
 // rrr : rra와 rrb를 동시에 실행한다.
 void reverse_rotate_ab(Node *ahead, Node *bhead)
@@ -33,5 +35,6 @@ void reverse_rotate_ab(Node *ahead, Node *bhead)
 	delete_tail(ahead, ahead->prev);
 	insert(bhead, bhead->prev->data);
 	delete_tail(bhead, bhead->prev);
-	write(1, "rrr\n", 4);
+	// write(1, "rrr\n", 4);
+	print_cmd(ahead, 9);
 }
