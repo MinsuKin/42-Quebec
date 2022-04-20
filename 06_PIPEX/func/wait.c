1f050bf0cd0083a4fdab44691527628b48daafb2
Minsu Kim <minkim@c3r9p2.42quebec.com>
2022-04-20 16:09:49 -0400

    push_swap_bug_fix

---
+++ 06_PIPEX/func/wait.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wait.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 17:11:59 by minkim            #+#    #+#             */
+/*   Updated: 2022/04/19 21:40:11 by minkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pipex.h"
+
+int main(void)
+{
+    pid_t pid;
+
+    pid = fork();
+
+    int wait_res;
+    int status;
+    
+    if (pid == 0)
+    {
+        printf("\n****자식프로세스****\n");
+        sleep(2);
+        printf("자식프로세스 종료\n");
+    }
+    else
+    {
+        wait_res = wait(&status);
+        printf("\n****부모프로세스****\n");
+        sleep(1);
+        printf("부모프로세스 종료\n");
+        printf("wait인자값: %d\nwait반환값: %d\n", status, wait_res);
+        printf("자식pid: %d\n", pid);
+    }
+    
+    // if (pid == 0)
+    // {
+    //     printf("\n****자식프로세스****\n");
+    //     sleep(2);
+    //     printf("자식프로세스 종료\n");
+    // }
+    // else
+    // {
+    //     printf("\n****부모프로세스****\n");
+    //     sleep(1);
+    //     printf("부모프로세스 종료\n");
+    // }
+}
