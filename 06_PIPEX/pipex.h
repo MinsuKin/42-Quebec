41c2baa3ae5523008a5cbad3769280bcb22a9228
Minsu <minus9521@gmail.com>
2022-04-30 20:14:55 -0400

    04.30_@

--- 06_PIPEX/pipex.h
+++ 06_PIPEX/pipex.h
@@ -6,7 +6,7 @@
 /*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 17:16:36 by minkim            #+#    #+#             */
-/*   Updated: 2022/04/30 14:31:51 by minkim           ###   ########.fr       */
+/*   Updated: 2022/04/30 19:31:50 by minkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@
 # include <string.h>
 # include <fcntl.h>
 
-size_t		ft_strlen(const char *s);
-char		*ft_strjoin(char const *s1, char const *s2);
-char		**ft_split(char const *s, char c);
-int			ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	ft_error(int exit_code);
+void	ft_exe(char *argv, char **envp);
 
 #endif
---
+++ 06_PIPEX/pipex_util.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_util.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/30 19:28:49 by minkim            #+#    #+#             */
+/*   Updated: 2022/04/30 20:09:25 by minkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	ft_error(int exit_code)
+{
+	perror("Error");
+	exit(exit_code);
+}
+
+static char	*ft_path(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp("PATH=", envp[i], 5) == 0)
+			return (envp[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+static char	*ft_cmd(char *cmd, char **envp)
+{
+	char	**path;
+	char	*input;
+	int		i;
+
+	path = ft_split(ft_path(envp)+5, ':');
+	if (path == 0)
+		ft_error(1);
+	i = 0;
+	while (path[i])
+	{
+		input = ft_strjoin(ft_strjoin(path[i], "/"), cmd);
+		if (access(input, X_OK) == 0)
+			return (input);
+		i++;
+	}
+	return (NULL);
+}
+
+void	ft_exe(char *argv, char **envp)
+{
+	char	*cmd;
+	char	**option;
+
+	option = ft_split(argv, ' ');
+	cmd = ft_cmd(option[0], envp);
+	if (option == 0)
+		ft_error(1);
+	if (execve(cmd, option, envp) == -1)
+		ft_error(1);
+}
