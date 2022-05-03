baf5456584de911b54a1abd8a28a81dd4acb25e2
Minsu <minus9521@gmail.com>
2022-05-03 16:12:26 -0400

    5.3

--- 06_PIPEX/pipex.c
+++ 06_PIPEX/pipex.c
@@ -6,7 +6,7 @@
 /*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 17:11:59 by minkim            #+#    #+#             */
-/*   Updated: 2022/05/02 21:04:55 by minkim           ###   ########.fr       */
+/*   Updated: 2022/05/03 16:10:49 by minkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_child(char **argv, char **envp, int *fd, int infile)
 	ft_exe(argv[2], envp);
 }
 
-void	ft_parent(char **argv, char **envp, int *fd, int outfile)
+void	ft_child2(char **argv, char **envp, int *fd, int outfile)
 {
 	dup2(fd[0], STDIN_FILENO);
 	dup2(outfile, STDOUT_FILENO);
@@ -34,24 +34,25 @@ int	main(int argc, char **argv, char **envp)
 	int		infile;
 	int		outfile;
 	pid_t	pid;
+	pid_t	pid2;
 
 	if (argc == 5)
 	{
-	infile = open(argv[1], O_RDONLY, 0644);
-	outfile = open(argv[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
+		infile = open(argv[1], O_RDONLY, 0644);
+		outfile = open(argv[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
 		if (infile == -1 || outfile == -1)
 			ft_error(1);
 		if (pipe(fd) == -1)
 			ft_error(1);
 		pid = fork();
-		if (pid == -1)
-			ft_error(1);
 		if (pid == 0)
 			ft_child(argv, envp, fd, infile);
-		else
-			ft_parent(argv, envp, fd, outfile);
+		pid2 = fork();
+		if (pid2 == 0)
+			ft_child2(argv, envp, fd, outfile);
+		waitpid(pid, 0, 0);
+		waitpid(pid2, 0, 0);
 	}
 	else
 		ft_error(1);
-	return (0);
 }
