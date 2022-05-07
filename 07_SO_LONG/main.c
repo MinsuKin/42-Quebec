fa537c5df8274a11e1a698c770acf3f2774e7bd1
Minsu <minus9521@gmail.com>
2022-05-07 16:49:35 -0400

    05.07_2

--- 07_SO_LONG/main.c
+++ 07_SO_LONG/main.c
@@ -6,7 +6,7 @@
 /*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 20:25:09 by minkim            #+#    #+#             */
-/*   Updated: 2022/05/07 14:04:03 by minkim           ###   ########.fr       */
+/*   Updated: 2022/05/07 16:39:01 by minkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void			param_init(t_param *param)
 {
-	param->x = 3;
-	param->y = 4;
+	param->x = 1;
+	param->y = 1;
 }
 
 int				key_press(int keycode, t_param *param)
@@ -23,15 +23,15 @@ int				key_press(int keycode, t_param *param)
 	// static int a = 0;
 
 	if (keycode == KEY_W)
-		param->y++;
+		param->y++; //move_w(param);
 	else if (keycode == KEY_S)
-		param->y--;
+		param->y--; //move_s(param);
 	else if (keycode == KEY_A)
-		param->x--;
+		param->x--; //move_a(param);
 	else if (keycode == KEY_D)
-		param->x++;
+		param->x++; //move_d(param);
 	else if (keycode == KEY_ESC)
-		exit(0);
+		exit(0); //exit_game(param);
 	printf("x: %d, y: %d\n", param->x, param->y);
 	return (0);
 }
@@ -210,10 +210,10 @@ int			main(void)
 	win = mlx_new_window(mlx, 1000, 500, "my_mlx");
 	img = mlx_xpm_file_to_image(mlx, "./images/snow-tilemap.xpm", &img_width, &img_height);
 	img2 = mlx_xpm_file_to_image(mlx, "./images/icecrystal.xpm", &img_width, &img_height);
-	img3 = mlx_xpm_file_to_image(mlx, "./images/character.xpm", &img_width, &img_height);
-	img4 = mlx_xpm_file_to_image(mlx, "./images/icecrystal.xpm", &img_width, &img_height);
-	img5 = mlx_xpm_file_to_image(mlx, "./images/ice-sword.xpm", &img_width, &img_height);
-	img6 = mlx_xpm_file_to_image(mlx, "./images/star-sword.xpm", &img_width, &img_height);
+	img3 = mlx_xpm_file_to_image(mlx, "./images/ice-sword.xpm", &img_width, &img_height);
+	img4 = mlx_xpm_file_to_image(mlx, "./images/icebox.xpm", &img_width, &img_height);
+	img5 = mlx_xpm_file_to_image(mlx, "./images/character.xpm", &img_width, &img_height);
+	img6 = mlx_xpm_file_to_image(mlx, "./images/path-tilemap.xpm", &img_width, &img_height);
 	img7 = mlx_xpm_file_to_image(mlx, "./images/fire-sword.xpm", &img_width, &img_height);
 	
 
@@ -236,18 +236,22 @@ int			main(void)
 			mlx_put_image_to_window(mlx, win, img, x * 64, y * 64);
 			if (two_d_map[y][x] == '1')
 				mlx_put_image_to_window(mlx, win, img2, x * 64, y * 64);
+			else if (two_d_map[y][x] == 'C')
+				mlx_put_image_to_window(mlx, win, img3, x * 64, y * 64);
+			else if (two_d_map[y][x] == 'E')
+				mlx_put_image_to_window(mlx, win, img4, x * 64, y * 64);
+			else if (two_d_map[y][x] == 'P')
+			{
+				mlx_put_image_to_window(mlx, win, img5, x * 64, y * 64);
+				param.x = x;
+				param.y = y;
+			}
 			x++;
 		}
 		y++;
 	}
 
 
-	// mlx_put_image_to_window(mlx, win, img2, 64, 0);
-	// mlx_put_image_to_window(mlx, win, img3, 128, 0);
-	// mlx_put_image_to_window(mlx, win, img4, 192, 64);
-	// mlx_put_image_to_window(mlx, win, img5, 0, 64);
-	// mlx_put_image_to_window(mlx, win, img6, 64, 64);
-	// mlx_put_image_to_window(mlx, win, img7, 128, 64);
 	mlx_hook(win, X_EVENT_KEY_RELEASE, 0, &key_press, &param);
 	mlx_loop(mlx);
 	return (0);
