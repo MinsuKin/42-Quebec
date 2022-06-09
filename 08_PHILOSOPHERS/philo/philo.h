f1f8ed7bdcc7b86c2ac0588561a33a9f5db10613
Minsu Kim <minus9521@gmail.com>
2022-06-08 20:42:53 -0400

    06.08

--- 08_PHILOSOPHERS/philo/philo.h
+++ 08_PHILOSOPHERS/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 19:46:21 by minkim            #+#    #+#             */
-/*   Updated: 2022/05/31 15:19:25 by minkim           ###   ########.fr       */
+/*   Updated: 2022/06/04 15:25:32 by minkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ typedef struct s_args
 	size_t	t_eat; //time_to_eat;
 	size_t	t_sleep; //time_to_sleep;
 	int	n_must_eat; //number_of_times_each_philosopher_must_eat;
+	int eating_cnt;
+	int finish;
 }	t_args;
 
 typedef struct s_lock
@@ -42,7 +44,7 @@ typedef struct s_phil
 	t_lock lock;
 	pthread_t tid;
 	int my_num;
-	int eating_cnt;
+	size_t last_eat_time;
 }	t_phil;
 
 int	ft_atoi(const char *str);
---
+++ 09_MINISHELL/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minkim <minkim@student.42quebec.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 13:40:49 by minkim            #+#    #+#             */
+/*   Updated: 2022/06/08 18:22:03 by minkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
