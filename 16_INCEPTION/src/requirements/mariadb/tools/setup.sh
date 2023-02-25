cd8dc418cf4a00daee03857440c4b6f5d739b971
Minsu <minus9521@gmail.com>
2023-02-24 19:24:42 -0500

    mariadb finish

--- 16_INCEPTION/src/requirements/mariadb/tools/setup.sh
+++ 16_INCEPTION/src/requirements/mariadb/tools/setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-mysql -e "CREATE USER '${MARIA_ID}'@'localhost' IDENTIFIED BY '${MARIA_PW}';";
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO'${MARIA_ID}'@'%' IDENTIFIED BY '${MARIA_PW}';";
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MARIA_PW}') WHERE USER = 'root';"; &&\
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
 
-cd /mysql/bin
+    mysql -e "CREATE USER '${MARIA_ID}'@'localhost' IDENTIFIED BY '${MARIA_PW}';" &&\
+    mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_ID}'@localhost IDENTIFIED BY '${MARIA_PW}';" &&\
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_ID}'@'%' IDENTIFIED BY '${MARIA_PW}';" &&\
+    mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MARIA_PW}') WHERE USER = 'root';" &&\ 
+    mysql -e "FLUSH PRIVILGES;"    
+    service mysql stop
+fi
+
+sleep 3
+
 mysqld
