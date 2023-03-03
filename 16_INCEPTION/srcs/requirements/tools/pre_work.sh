7d7c5d5997ecd0a50761ca8afc0c4026bd29e744
Minsu <minus9521@gmail.com>
2023-03-03 12:31:22 -0500

    mariadb exit error fix

--- 16_INCEPTION/srcs/requirements/tools/pre_work.sh
+++ 16_INCEPTION/srcs/requirements/tools/pre_work.sh
@@ -2,20 +2,20 @@
 
 echo -e "\e[47;35m--Remove images and containers--\e[0m\n"
 
-docker-compose down
+docker-compose down 
 docker system prune -a
 
 echo -e "\e[44;33mIt's all been erased\e[0m\n\n"
 
-# load environment variables
+echo -e "\e[47;35m--Setting for environment variable load--\e[0m\n"
+
 source ../../.env
 
-echo -e "\e[44;33It's set up\e[0m\n\n"
+echo -e "\e[44;33mIt's set up\e[0m\n\n"
 
 echo -e "\e[47;35m--Host conversion operation to replace DNS--\e[0m\n"
 
-# Add DNS like localhost to change domain
-echo '127.0.0.1 minkim.42.fr' >> /etc/hosts
-echo '127.0.0.1 www.minkim.42.fr' >> /etc/hosts
+echo '127.0.0.1 minkim.42.fr' >> /etc/hosts 
+echo '127.0.0.1 www.minkim.42.fr' >> /etc/hosts 
 
 echo -e "\e[44;33mHost is set up\e[0m\n\n"
--- 16_INCEPTION/srcs/requirements/wordpress/tools/setup.sh
+++ 16_INCEPTION/srcs/requirements/wordpress/tools/setup.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
-# sleep 10
 for ((i=10000; i>=0; i--))
 do
-    echo -e "\e[31mloading...\e[0m\n"
+	echo -e "\e[31mloading...\e[0m\n"
 done
 
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
+if  [ ! -f /var/www/wordpress/wp-config.php ]; then 
+    
+	for ((i=10000; i>=0; i--))
+	do
+		echo -e "\e[31mloading...\e[0m\n"
+	done
 
-    for ((i=10000; i>=0; i--))
-    do
-        echo -e "\e[31mloading...\e[0m\n"
-    done
-
-    wp core --allow-root download --locale=en_CA --force
-
-    while [ ! -f /var/www/wordpress/wp-config.php ]; do
+    wp core --allow-root download --locale=ko_KR --force 
+    while  [ ! -f /var/www/wordpress/wp-config.php ]; do
         # allow wp-cli admin auth, setup db admin
-        wp core config --allow-root \
-            --dbname=wordpress --dbuser=${MARIA_ID} --dbpass=${MARIA_PW} --dbhost=mariadb:3306
-    done
+        wp core config --allow-root --dbname=wordpress --dbuser=${MARIA_ID} --dbpass=${MARIA_PW} --dbhost=mariadb:3306
 
+	done
     # create wordpress table in database
-    wp core install config --allow-root \
-        --url='minkim.42.fr' --title='Inception' \
-        --admin_user=${WP_ID} --admin_password=${WP_PW} --admin_email="minkim@student.42quebec.com" \
-        --path='/var/www/wordpress';
-
+    wp core install --allow-root --url='minkim.42.fr' --title='Inception' --admin_user=${WP_ID} --admin_password=${WP_PW}  --admin_email="minkim@student.42quebec.com" --path='/var/www/wordpress';
     # create user
-    wp user create --allow-root \
-        ${WP_USER_ID} minus9521@gmail.com --user_pass=${WP_USER_PW} --role=author
-fi
+    wp user create --allow-root ${WP_USER_ID} minus9521@gmail.com --user_pass=${WP_USER_PW} --role=author 
+fi 
 
 # not sending process to background, pid remains 1 while the process stays in foreground
 php-fpm7.3 --nodaemonize
