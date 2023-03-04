e63f1056dec79b3b3b7d24ff41d4936582e04304
Minsu <minus9521@gmail.com>
2023-03-04 10:35:22 -0500

    inception added mysql shutdown

--- 16_INCEPTION/srcs/requirements/mariadb/tools/setup.sh
+++ 16_INCEPTION/srcs/requirements/mariadb/tools/setup.sh
@@ -5,15 +5,14 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
     mysql_install_db
     service mysql start
     
-    mysql -e "CREATE USER '${MARIA_ID}'@'localhost' IDENTIFIED by '${MARIA_PW}';" &&\
-	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_ID}'@localhost IDENTIFIED BY '${MARIA_PW}';" &&\
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_ID}'@'%' IDENTIFIED BY '${MARIA_PW}';" &&\
-	mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MARIA_PW}') WHERE User = 'root';" &&\
+    mysql -e "CREATE USER '${MARIA_ID}'@'localhost' IDENTIFIED BY '${MARIA_PW}';"
+	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_ID}'@'localhost' IDENTIFIED BY '${MARIA_PW}';"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_ID}'@'%' IDENTIFIED BY '${MARIA_PW}';"
+	mysql -e "UPDATE mysql.user SET password = PASSWORD('${MARIA_PW}') WHERE user = 'root';"
 	mysql -e "FLUSH PRIVILEGES;"
-    service mysql stop 
+
+    mysqladmin -uroot -p${MARIA_PW} shutdown
 fi
 
-sleep 3
-
-mysqld
+exec mysqld_safe
